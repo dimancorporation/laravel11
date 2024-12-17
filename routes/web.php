5c4a50a7749c0331e7683856ce906df045912f68
vlfred88 <valfred88@gmail.com>
2024-12-17 18:58:01 +0300

    feat: обновление конфигураций и маршрутов
    - добавлен пример APP_KEY в .env.example
    - переключение DB_CONNECTION на PostgreSQL, поддержка Redis
    - активация Xdebug в docker-compose
    - добавлен маршрут /test с использованием BitrixController

--- routes/web.php
+++ routes/web.php
@@ -1,11 +1,14 @@
 <?php
 
+use App\Http\Controllers\BitrixController;
 use App\Http\Controllers\ProfileController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/test', [BitrixController::class, 'getUserList'])->name('bitrix');
 
 Route::get('/dashboard', function () {
     return view('dashboard');
