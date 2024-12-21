c39d2606870e31f153ca3d5dd09d2ec4754a756b
d.novikov <d.novikov@bars.group>
2024-12-21 19:37:56 +0700

    тестовые методы для получения данных из битрикс24 и последующего сохранения в бд

--- app/Http/Controllers/BitrixController.php
+++ app/Http/Controllers/BitrixController.php
@@ -29,11 +29,41 @@ class BitrixController extends Controller
 //        ])->getId();
 //        print_r($response);
 //
-//        $response = $this->serviceBuilder->getCRMScope()->deal()->get(3);
-//        $response = $this->serviceBuilder->getCRMScope()->core->call('crm.deal.get', ['ID' => 3]);
-//        dd([
-//            'данные по сделке' => $response,
-//        ]);
+        $response = $this->serviceBuilder->getCRMScope()->deal()->get(3)->deal();
+        /*
+        UF_CRM_1708511654449 - СОЗДАТЬ ЛК КЛИЕНТУ
+        UF_CRM_1708511589360 - ЛОГИН ЛК КЛИЕНТА
+        UF_CRM_1708511607581 - ПАРОЛЬ ЛК КЛИЕНТА
+        UF_CRM_1709533755311 - СТАТУС ДЛЯ ЛК КЛИЕНТА
+        UF_CRM_1725026451112 - СУММА ДОГОВОРА
+        UF_CRM_1708511318200 - СООБЩЕНИЕ КЛИЕНТУ ОТ КОМПАНИИ
+        UF_CRM_1708511472339 - ССЫЛКА НА ДЕЛО В КАДР. АРБИТР
+        UF_CRM_1715524078722 - Дата последней авторизации (МСК)
+        CONTACT_ID - айди контакта
+         */
+
+        //$response = $this->serviceBuilder->getCRMScope()->contact()->get(5)->contact();
+        /*
+        "NAME" => "тестовый" - имя
+        "SECOND_NAME" => null - отчество
+        "LAST_NAME" => "тест тестович" - фамилия
+        ["PHONE"][0]["VALUE"]
+         */
+
+        //https://dev.1c-bitrix.ru/api_d7/bitrix/crm/crm_owner_type/identifiers.php
+        //для счетов entityTypeId = 31
+        $response = $this->serviceBuilder->getCRMScope()->item()->list(
+            31,
+            [],
+            ['contactId' => 7],
+            ['*']
+//            ['id', 'title', 'opportunity', 'isManualOpportunity', 'currencyId', 'createdTime', 'updatedTime', 'movedTime', 'stageId', 'previousStageId', 'begindate', 'closedate', 'categoryId', 'contactId']
+        )->getItems();
+        //"categoryId" => 3 ???
+        //"stageId" => "DT31_3:P"
+        //"opportunity" => 10000
+        //"isManualOpportunity" => "Y"
+        dump($response);
 
         $response = $this->serviceBuilder->getCRMScope();
 
