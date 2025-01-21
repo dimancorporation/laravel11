238a1e9664b131925408939d3b3ac50442e69cef
s.vilkov <s.vilkov@bars.group>
2025-01-21 21:25:50 +0300

    refactor(payment-service): удалить закомментированный неиспользуемый код

--- app/Services/PaymentService.php
+++ app/Services/PaymentService.php
@@ -53,25 +53,6 @@ class PaymentService
         } catch (ModelNotFoundException $e) {
             dump($e->getMessage());
         }
-
-        // can use $this->findUser($request) or use relations
-        // need replace for $request->validated()
-//        Payment::create([
-//            'b24_deal_id' => $user->id_b24,
-//            'b24_contact_id' => $user->contact_id,
-//            'b24_invoice_id' => null,
-//            'order_id' => $data['OrderId'],
-//            'success' => $data['Success'],
-//            'status' => $data['Status'],
-//            'payment_id' => $data['PaymentId'],
-//            'amount' => $data['Amount'] / 100,
-//            'card_id' => $data['CardId'],
-//            'email' => $data['Data']['Email'],
-//            'name' => $data['Data']['Name'],
-//            'phone' => '+7' . $data['Data']['Phone'],
-//            'source' => $data['Data']['Source'],
-//            'user_agent' => $data['Data']['user_agent'],
-//        ]);
     }
 
     public function updateExistingPayment(Payment $payment, array $data): void
