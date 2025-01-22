49eefa65e025189093e50980b76b26989e42c15d
d.novikov <d.novikov@bars.group>
2025-01-22 16:10:01 +0700

    русификация

--- lang/ru/auth.php
+++ lang/ru/auth.php
@@ -13,8 +13,8 @@ return [
     |
     */
 
-    'failed' => 'These credentials do not match our records.',
-    'password' => 'The provided password is incorrect.',
-    'throttle' => 'Too many login attempts. Please try again in :seconds seconds.',
+    'failed' => 'Эти учетные данные не совпадают с нашими записями.',
+    'password' => 'Указанный пароль неверен.',
+    'throttle' => 'Слишком много попыток входа. Попробуйте снова через :seconds секунд.',
 
 ];
