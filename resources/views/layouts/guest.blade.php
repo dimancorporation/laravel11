09a71ef23a6bfa48b81c6dd2834db520cabc21c0
d.novikov <d.novikov@bars.group>
2025-03-04 11:31:29 +0700

    убрал дебаг инфу об активной теме

--- resources/views/layouts/guest.blade.php
+++ resources/views/layouts/guest.blade.php
@@ -17,7 +17,6 @@
     @php
         $activeTheme = session('active_theme', 'default');
     @endphp
-    {{ $activeTheme }}
     <body class="font-sans text-gray-900 antialiased {{ $activeTheme }}-theme">
         <div class="min-h-screen flex flex-col sm:justify-center items-center pt-6 sm:pt-0 bg-gray-100">
             <div>
