99b9ffec09b7da37d0f953561f35daf1d0691467
d.novikov <d.novikov@bars.group>
2025-03-06 08:44:21 +0700

    добавлен пояснительный текст по изменению темы(тема хранится в кеше 1ч)

--- resources/views/settings.blade.php
+++ resources/views/settings.blade.php
@@ -28,7 +28,7 @@
 
                         <form method="post" action="{{ route('admin.theme.update') }}" class="mt-6 space-y-6">
                             @csrf
-                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="theme_name">Выберите тему:</label>
+                            <label class="block mb-2 text-sm font-medium text-gray-900 dark:text-white" for="theme_name">Выберите тему: (изменения вступят в силу через 1 час)</label>
                             <select name="theme_name" id="theme_name" class="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500">
                                 @foreach($themes as $theme)
                                     @php
