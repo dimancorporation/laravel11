1e04b6445b4f7d4f96725a2a495392ee2061ea91
d.novikov <d.novikov@bars.group>
2025-03-21 11:58:29 +0700

    cors

--- config/cors.php
+++ config/cors.php
@@ -17,8 +17,13 @@ return [
 
     'paths' => ['api/*'], // Маршрут, на котором действует CORS
 
+    //'allowed_methods' => ['POST'], // Разрешаем только POST-запросы
     'allowed_methods' => ['*'], // Разрешённые методы
 
+    'allowed_origins' => ['*'], // Разрешённые источники (временно разрешаем всё)
+
+    'allowed_origins_patterns' => [], // Шаблон для разрешений
+    /*
     //'allowed_origins' => ['*'], // Разрешённые источники (временно разрешаем всё)
     'allowed_origins' => [
         'https://bankrotof.bitrix24.ru', // Разрешенный домен Bitrix24
@@ -28,7 +33,19 @@ return [
     'allowed_origins_patterns' => [
         'https://.*\.tinkoff\.ru', // Регулярное выражение для tinkoff.ru и всех его поддоменов
     ],
+    */
+    /*
+    // Явно разрешенные домены (без поддоменов)
+    'allowed_origins' => [
+        // Здесь можно указать конкретные домены, если нужно
+    ],
 
+    // Разрешенные домены и поддомены через регулярные выражения
+    'allowed_origins_patterns' => [
+        'https://.*\.tinkoff\.ru', // Разрешаем tinkoff.ru и все его поддомены
+        'https://.*\.bitrix24\.ru', // Разрешаем bitrix24.ru и все его поддомены
+    ],
+    */
     'allowed_headers' => ['*'], // Разрешённые заголовки
 
     'exposed_headers' => [], // Экспонируемые заголовки
