afbea30630fda461cb0d7636d988132ec570dc67
d.novikov <d.novikov@bars.group>
2025-04-07 22:06:21 +0700

    вынос времени кеширования темы в таблицу БД settings

--- app/Http/Middleware/SetActiveTheme.php
+++ app/Http/Middleware/SetActiveTheme.php
@@ -6,6 +6,7 @@ use Closure;
 use Illuminate\Support\Facades\Cache;
 use Illuminate\Support\Facades\Session;
 use App\Models\ThemeSetting;
+use App\Models\Setting;
 
 class SetActiveTheme
 {
@@ -13,13 +14,13 @@ class SetActiveTheme
     {
         // Проверяем кэш на наличие активной темы
         $activeTheme = Cache::get('active_theme');
+        $themeCacheDuration = Setting::getValueByCode('THEME_CACHE_DURATION') ?? 3600; // время кеширования в секундах (по умолчанию 3600 секунд = 1 час)
 
         if (!$activeTheme) {
             // Если нет, получаем из базы данных
             $activeTheme = ThemeSetting::where('is_active', true)->first();
             $activeThemeName = $activeTheme ? $activeTheme->theme_name : 'default';
-//            Cache::put('active_theme', $activeThemeName, 3600); // Кэшируем на 1 час
-            Cache::put('active_theme', $activeThemeName, 60); // Кэшируем на 1 минуту
+            Cache::put('active_theme', $activeThemeName, $themeCacheDuration);
         } else {
             $activeThemeName = $activeTheme;
         }
